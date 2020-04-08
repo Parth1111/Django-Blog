2320ce1a5f941bea836d3d1cc171170370d5054b
Parth1111 <parthsshah.97@gmail.com>
2020-04-08 20:42:00 +0530

    - Added Data Models

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-#dummy data to pass to templates
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post',
-        'date_posted': 'August 27 2018'
-    },
-    {
-        'author': 'ANC Jacob',
-        'title': 'Blog Post 2',
-        'content': 'Second post',
-        'date_posted': 'September 12 2018'
-    },
-    {
-        'author': 'Joe Exotic',
-        'title': 'Blog Post 3',
-        'content': 'Third post',
-        'date_posted': 'December 02 2018'
-    }
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
