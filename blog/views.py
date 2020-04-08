dace80defc174864a63c523f5208d7c553d134f5
Parth1111 <parthsshah.97@gmail.com>
2020-04-08 17:43:36 +0530

    - Added Templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
+#dummy data to pass to templates
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post',
+        'date_posted': 'August 27 2018'
+    },
+    {
+        'author': 'ANC Jacob',
+        'title': 'Blog Post 2',
+        'content': 'Second post',
+        'date_posted': 'September 12 2018'
+    },
+    {
+        'author': 'Joe Exotic',
+        'title': 'Blog Post 3',
+        'content': 'Third post',
+        'date_posted': 'December 02 2018'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request,'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
